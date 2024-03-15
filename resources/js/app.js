62e87f9c422e905bdd9903adf3eaf8b6f2ba213b
Byron Jimenez <byronjimenez9911@gmail.com>
2024-03-14 18:34:43 -0600

    Install and setup core ui pro

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,9 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import router from './router';
 import App from "./App.vue";
-import CoreuiVue from '@coreui/vue'
+import CoreuiVue from '@coreui/vue-pro'
 import CIcon from '@coreui/icons-vue'
+import '@coreui/coreui-pro/dist/css/coreui.min.css'
 import { iconsSet as icons } from '../icons/index'
 import { registerFirebaseMessagingServiceWorker } from './services/service-worker';
 import { Select2 } from "select2-vue-component";
